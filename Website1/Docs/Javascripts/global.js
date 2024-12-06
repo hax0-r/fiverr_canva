ff07004a66a0a3f047e6ac81275bac08b1ed7cd3
Muhammad <talha18513@gmail.com>
2024-12-06 09:58:07 +0500

    landing page done

---
+++ Website1/Docs/Javascripts/global.js
@@ -0,0 +1,33 @@
+const lenis = new Lenis();
+function raf(time) {
+    lenis.raf(time);
+    requestAnimationFrame(raf);
+}
+requestAnimationFrame(raf);
+
+
+const contactForm = document.getElementById("contactForm");
+const overlay = document.getElementById("overlay");
+const openContactForm = document.getElementsByClassName("openContactForm");
+
+Array.from(openContactForm).forEach(button => {
+    button.addEventListener('click', () => {
+        overlay.style.display = 'block';
+        contactForm.style.display = 'flex';
+
+        setTimeout(() => {
+            overlay.style.opacity = '1';
+            contactForm.style.opacity = '1';
+        }, 50);
+    });
+});
+
+overlay.addEventListener('click', () => {
+    overlay.style.opacity = '0';
+    contactForm.style.opacity = '0';
+
+    setTimeout(() => {
+        overlay.style.display = 'none';
+        contactForm.style.display = 'none';
+    }, 500);
+});
